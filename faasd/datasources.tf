5ca0a4b4bd7f42bc530b662ff6880bc476d3f10e
Engin Diri <engin.diri@mail.schwarz>
2021-05-14 22:59:13 +0200

    initial commit of the stackit faasd stack (#1)

---
+++ faasd/datasources.tf
@@ -0,0 +1,3 @@
+data "openstack_networking_network_v2" "floating" {
+  name = var.pool
+}
---
+++ faasd/variables.tf
@@ -0,0 +1,23 @@
+variable "pool" {
+  default = "floating-net"
+}
+
+variable "ssh_key_file" {
+  default = "ssh/faasd"
+}
+
+variable "faasd-name" {
+  default = "faasd"
+}
+
+variable "flavor" {
+  default = "c1.3"
+}
+
+variable "subnet-cidr" {
+  default = "10.1.10.0/24"
+}
+
+variable "ubuntu-image-id" {
+  default = "b017f5da-86e2-49ec-98ce-14250f758bfa"
+}
